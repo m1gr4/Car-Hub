cff93ae0ac98650cd0c93109731a53acba390d6d
m1gr4 <felipe.riquelme.m@gmail.com>
2023-11-06 14:02:25 +0000

    Pending changes exported from your codespace

---
+++ components/CustomButton.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { CustomButtonProps } from '@/types';
+import Image from 'next/image'
+
+const CustomButton = ({ title, containerStyle, handleClick, btnType }: CustomButtonProps) => {
+  return (
+    <button
+      disabled={true}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyle}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>
+        {title}
+      </span>
+
+    </button>
+  )
+}
+
+export default CustomButton;
